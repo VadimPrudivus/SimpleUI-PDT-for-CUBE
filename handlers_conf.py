010164bd939d0b48b2fa29d754fe3b9260e70bbf
VadimPrudivus <159469926+VadimPrudivus@users.noreply.github.com>
2025-08-18 11:57:04 +0300

    Update handlers_conf.py

--- handlers_conf.py
+++ handlers_conf.py
@@ -1,5 +1,10 @@
+import json
+
 def ws_handler(hashMap, _files=None, _data=None):
     message = hashMap.get("WebSocketMessage")
-    #data = json.loads(message)
-
-    #print(message)
+    if message:
+        data = json.loads(message)  # Преобразуем JSON строку в словарь
+        # Записываем в стек переменных SimpleUI
+        hashMap.put("user_id", data.get("id", ""))
+        hashMap.put("user_name", data.get("name", ""))
+        # Можно вернуть сообщение или просто выйти
