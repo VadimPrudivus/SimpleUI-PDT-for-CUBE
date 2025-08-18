fa25bed6605acb2a41543326a6a44425f12563fb
VadimPrudivus <159469926+VadimPrudivus@users.noreply.github.com>
2025-08-18 08:09:34 +0300

    Update handlers_conf.py

--- handlers_conf.py
+++ handlers_conf.py
@@ -1,6 +1,6 @@
 def ws_handler(hashMap, _files=None, _data=None):
     message = hashMap.get("WebSocketMessage")
-    toast(message)
+    print(message)
 
     # Можно изменить hashMap, например отправить ответ:
     #hashMap.put("WebSocketSend", '{"event":"response_from_ui"}')
