446497496fa230deaf537f0b41bb8c0d4b592d37
VadimPrudivus <159469926+VadimPrudivus@users.noreply.github.com>
2025-08-18 12:14:00 +0300

    Update handlers_conf.py

--- handlers_conf.py
+++ handlers_conf.py
@@ -1,10 +1,22 @@
 import json
 
 def ws_handler(hashMap, _files=None, _data=None):
+    if not hashMap:
+        # хэндлить случай с пустым hashMap, чтобы не обращаться к None
+        return
+
     message = hashMap.get("WebSocketMessage")
     if message:
-        data = json.loads(message)  # Преобразуем JSON строку в словарь
-        # Записываем в стек переменных SimpleUI
+        try:
+            data = json.loads(message)
+            # дальше работа с data
+        except Exception as ex:
+            # Логируем ошибку парсинга JSON
+            print(f"Ошибка JSON: {ex}")
+    else:
+        print("Сообщение WebSocketMessage отсутствует или пусто")
+
+# Записываем в стек переменных SimpleUI
         #hashMap.put("user_id", data.get("id", ""))
         #hashMap.put("user_name", data.get("name", ""))
         # Можно вернуть сообщение или просто выйти
