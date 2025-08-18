1f031beb90c153307a3b6624fa8eef0f88098304
VadimPrudivus <159469926+VadimPrudivus@users.noreply.github.com>
2025-08-18 11:28:05 +0300

    Update handlers_conf.py

--- handlers_conf.py
+++ handlers_conf.py
@@ -1,13 +1,5 @@
 def ws_handler(hashMap, _files=None, _data=None):
-    import json
     message = hashMap.get("WebSocketMessage")
     data = json.loads(message)
 
-    # Флаг, показывающий нужно ли отправлять ответ
-    #send_response = (data.get("event")!= "response_from_ui")
-
-    #if send_response:
-#        response = {"event": "response_from_ui", "received": data}
-#        hashMap.put("WebSocketSend", json.dumps(response))
-
-    return hashMap
+    toast(message)
