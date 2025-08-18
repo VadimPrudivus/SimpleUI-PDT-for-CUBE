a803251815f8442dbb8e209b114d55bd46e70090
VadimPrudivus <159469926+VadimPrudivus@users.noreply.github.com>
2025-08-18 12:08:04 +0300

    Update handlers_conf.py

--- handlers_conf.py
+++ handlers_conf.py
@@ -5,6 +5,6 @@ def ws_handler(hashMap, _files=None, _data=None):
     if message:
         data = json.loads(message)  # Преобразуем JSON строку в словарь
         # Записываем в стек переменных SimpleUI
-        hashMap.put("user_id", data.get("id", ""))
-        hashMap.put("user_name", data.get("name", ""))
+        #hashMap.put("user_id", data.get("id", ""))
+        #hashMap.put("user_name", data.get("name", ""))
         # Можно вернуть сообщение или просто выйти
