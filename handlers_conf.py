a770d68d4f0c80e5e1f197da7aeb7cbd601545d1
VadimPrudivus <159469926+VadimPrudivus@users.noreply.github.com>
2025-08-19 13:31:22 +0300

    Update handlers_conf.py

--- handlers_conf.py
+++ handlers_conf.py
@@ -7,20 +7,15 @@ def ws_handler(hashMap, _files=None, _data=None):
     message = hashMap.get("WebSocketMessage")
 
     if message:
-        try:
-            data = json.loads(message)
-            hashMap.put("toast", str(data))
+        data = json.loads(message)
+        hashMap.put("toast", str(data))
 
-            # Проверяем, что это событие входа
-            if data.get("event") == "login":
-                hashMap.put("user_id", data.get("id", ""))
-                hashMap.put("user_name", data.get("name", ""))
-        
-        except Exception as ex:
-            # Логируем ошибку парсинга JSON
-            print(f"Ошибка JSON: {ex}")
+        # Проверяем, что это событие входа
+        if data.get("event") == "login":
+            hashMap.put("user_id", data.get("id", ""))
+            hashMap.put("user_name", data.get("name", ""))
     else:
-        print("Сообщение WebSocketMessage отсутствует или пусто")
+        hashMap.put("toast", "Сообщение WebSocketMessage отсутствует или пусто")
 
     hashMap.put("RefreshScreen", "")
 
