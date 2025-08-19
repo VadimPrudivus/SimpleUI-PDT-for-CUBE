711fb27e392e035107b8b64ccb86b9395cff83d5
VadimPrudivus <159469926+VadimPrudivus@users.noreply.github.com>
2025-08-19 17:43:43 +0300

    Update handlers_conf.py

--- handlers_conf.py
+++ handlers_conf.py
@@ -6,7 +6,7 @@ def ws_connect(hashMap, _files=None, _data=None):
 def ws_handler(hashMap, _files=None, _data=None):
     message = hashMap.get("WebSocketMessage")
     hashMap.put("toast", message)
-
+'''
     if message:
         data = json.loads(message)
         #hashMap.put("toast", str(data))
@@ -19,7 +19,7 @@ def ws_handler(hashMap, _files=None, _data=None):
             hashMap.put("toast", hashMap.get("user_name"))
     else:
         hashMap.put("toast", "Сообщение WebSocketMessage отсутствует или пусто")
-
-    hashMap.put("RefreshScreen", "")
+'''
+    #hashMap.put("RefreshScreen", "")
 
     return hashMap
